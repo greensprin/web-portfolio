036d425d3524ee681a5208b646792c4f69352115
masashi momiki <mngjxd03170317@gmail.com>
2020-12-28 10:38:13 +0900

    画像のスライドに対応 ヘッダーのレスポンシブ対応中

--- myPortFolio13/js/script.js
+++ myPortFolio13/js/script.js
@@ -5,5 +5,31 @@ $(function() {
   // リサイズされたときのheaderの高さを決定
   $(window).resize(function() {
     $("header").css("height", $(window).height());
+  });
+
+  // 一番上に移動する
+  $(".goto-top").on("click", function() {
+    $("html, body").animate({scrollTop:0});
+    // $("html, body").animate({scrollTop:$("").offset().top});
+  });
+
+  $("header").bgSwitcher({
+    images: ["img/main_visual_111-0x0.jpg", "img/main_visual_13-0x0.jpg", "img/main_visual_7-0x0.jpg", "img/main_visual_6-0x0.jpg", "img/main_visual_2-0x0.jpg"],
+    // Interval: 5000,
+    // start: true,
+    // loop: true,
+    // shuffle: false,
+    // effect: "fade",
+    // duration: 1000,
+    // easing: "swing",
+  });
+
+  $(".hbg-menu").on("click", function() {
+    if ($(".hbg-menu").hasClass("show") === true) {
+      $("header .content-right ul").slideUp();
+    } else {
+      $("header .content-right ul").slideDown();
+    }
+    $(".hbg-menu").toggleClass("show");
   })
 })

f288691bd7b67682ad3d3fb752bace0a804e868f
mmkmss <mngjxd03170317@gmail.com>
2020-12-31 08:55:41 +0900

    header部のレスポンシブ対応

--- myPortFolio13/js/script.js
+++ myPortFolio13/js/script.js
@@ -2,9 +2,11 @@ $(function() {
   // headerの高さを決める //
   // 初回のheader高さを決定
   $("header").css("height", $(window).height());
+  $("header").css("width", $(window).width());
   // リサイズされたときのheaderの高さを決定
   $(window).resize(function() {
     $("header").css("height", $(window).height());
+    $("header").css("width", $(window).width());
   });
 
   // 一番上に移動する

7a18ef2db6cce3c8ce26cbc03cdb572d817357b8
mmkmss <mngjxd03170317@gmail.com>
2021-01-05 23:26:42 +0900

    自分のHPを追加

---
+++ myPortFolio15_myHP/js/script.js
@@ -0,0 +1,45 @@
+$(function() {
+  // headerのサイズ(px)
+  // var header_size = 80;
+  var header_size = $("header .container").height() + 10;
+
+  // 各要素の場所に移動
+  // worksへ移動
+  $("header .li-works").on("click", function() {
+    // 移動先の要素の座標を取得
+    var pos = $("#works").offset().top - header_size;
+    $("html, body").animate({scrollTop:pos});
+  });
+
+  // skillsへ移動
+  $("header .li-skills").on("click", function() {
+    // 移動先の要素の座標を取得
+    var pos = $("#skills").offset().top - header_size;
+    $("html, body").animate({scrollTop:pos});
+  });
+
+  // contactへ移動
+  $("header .li-contact").on("click", function() {
+    // 移動先の要素の座標を取得
+    var pos = $("#contact").offset().top - header_size;
+    $("html, body").animate({scrollTop:pos});
+  });
+
+  // ある程度スクロールしたら最上位へ移動するボタン表示
+  // 現在のスクロール位置取得
+  $(window).scroll(function() {
+    var culScrollPos = $(window).scrollTop();
+    // console.log(culScrollPos);
+    if (culScrollPos > 100) {
+      $(".goto-top").slideDown();
+    } else {
+      $(".goto-top").slideUp();
+    }
+
+  })
+
+  $(".goto-top").on("click", function() {
+    // ページ先頭に移動
+    $("html, body").animate({scrollTop:0});
+  });
+})
